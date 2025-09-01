d099014250980a212bdc23642b7713d2d2e3e197
anshika.dubey <anshika.dubey@CDHL-5CD146N29R>
2025-09-01 15:17:48 +0530

    Some modification

--- PracticeProject/src/test/java/practicePackage/ExercisePageTest.java
+++ PracticeProject/src/test/java/practicePackage/ExercisePageTest.java
@@ -8,8 +8,6 @@ import org.testng.annotations.Test;
 
 import baseClass.BaseClass2;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class ExercisePageTest extends BaseClass2 {
 
