e0072fb42509db37caa513b834368865254abccc
anshika.dubey <anshika.dubey@FLD-5CD146N29R>
2026-03-31 09:45:37 +0530

    new commit

--- PracticeProject/src/test/java/practicePackage/FullScreenTimeBasedScenarioTest.java
+++ PracticeProject/src/test/java/practicePackage/FullScreenTimeBasedScenarioTest.java
@@ -27,10 +27,10 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true))"
 				+ ".scrollIntoView(new UiSelector().text(\"In the past 7 days, how many days did you cough up mucus?\"))"));
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
 
 		// Click dropdown (Select button of specific question)
-		WebElement dropdowns1 = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
+		WebElement dropdowns1 = wait1.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
 				"//android.widget.TextView[contains(@text,'In the past 7 days, how many days did symptoms wake you at night?')]"
 				+ "/ancestor::android.view.ViewGroup[2]//android.view.ViewGroup[@content-desc='Select']")));
 
@@ -43,7 +43,7 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 
 		for (int i = 0; i < 5; i++) {
 		    try {
-		        WebElement value = wait.until(
+		        WebElement value = wait1.until(
 		                ExpectedConditions.visibilityOfElementLocated(valueLocator)
 		        );
 		        value.click();
@@ -56,32 +56,35 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 		
 //		option1.click();
 		
+		WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(5));
 		//Second question
-		WebElement dropdowns2 = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
+		WebElement dropdowns2 = wait2.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
 				"//android.widget.TextView[contains(@text,'In the past 7 days, how many days did you cough up mucus?')]"
 				+ "/ancestor::android.view.ViewGroup[2]//android.view.ViewGroup[@content-desc='Select']")));
 
 		dropdowns2.click();
 
 	    //Wait for options list & select value
-		By valueLocator1 = AppiumBy.xpath("//android.widget.TextView[@text='5']");
+		By valueLocator1 = AppiumBy.xpath("//android.widget.TextView[@text='3']");
 
 		
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 3; i++) {
 		    try {
-		        WebElement value1 = wait.until(
+		        WebElement value1 = wait2.until(
 		                ExpectedConditions.visibilityOfElementLocated(valueLocator1)
 		        );
 		        value1.click();
 		        break;
 		    } catch (StaleElementReferenceException e) {
+		    	
 		        System.out.println("Retrying due to stale element...");
 		    }
 		}
 		
+		WebDriverWait wait3 = new WebDriverWait(driver, Duration.ofSeconds(10));
 		//third question
-		WebElement dropdowns3 = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
+		WebElement dropdowns3 = wait3.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
 				"//android.widget.TextView[contains(@text,'In the past 7 days, how many days did your symptoms stop you from attending school, work, or doing your usual daily activities?')]"
 				+ "/ancestor::android.view.ViewGroup[2]//android.view.ViewGroup[@content-desc='Select']")));
 
@@ -93,7 +96,7 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 		
 		for (int i = 0; i < 7; i++) {
 		    try {
-		        WebElement value2 = wait.until(
+		        WebElement value2 = wait3.until(
 		                ExpectedConditions.visibilityOfElementLocated(valueLocator2)
 		        );
 		        value2.click();
@@ -103,8 +106,9 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 		    }
 		}
 		
+		WebDriverWait wait4 = new WebDriverWait(driver, Duration.ofSeconds(10));
 		// fourth question
-		WebElement dropdowns4 = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
+		WebElement dropdowns4 = wait4.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
 				"//android.widget.TextView[contains(@text,'In the past 7 days, how many days did you use your rescue inhaler?')]"
 						+ "/ancestor::android.view.ViewGroup[2]//android.view.ViewGroup[@content-desc='Select']")));
 
@@ -116,7 +120,7 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 		
 		for (int i = 0; i < 7; i++) {
 		    try {
-		        WebElement value4 = wait.until(
+		        WebElement value4 = wait4.until(
 		                ExpectedConditions.visibilityOfElementLocated(valueLocator4)
 		        );
 		        value4.click();
@@ -130,12 +134,13 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 				+ ".scrollIntoView(new UiSelector().text(\"In the past 7 days, how many days did you use your rescue inhaler?\"))"));
 		
 		// Fifth question
-		WebElement dropdowns5 = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
+		WebElement dropdowns5 = wait1.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath(
 				"//android.widget.TextView[contains(@text,'Were your symptoms triggered by physical activity, stress, weather change, or allergen exposure during the past 7 days (dust, pollen, pets)?')]"
 						+ "/ancestor::android.view.ViewGroup[2]//android.view.ViewGroup[@content-desc='Select']")));
 
 		dropdowns5.click();
 
+		WebDriverWait wait5 = new WebDriverWait(driver, Duration.ofSeconds(10));
 		// Wait for options list & select value
 		By valueLocator5 = AppiumBy.xpath("//android.widget.TextView[@text='3']");
 
@@ -143,7 +148,7 @@ public class FullScreenTimeBasedScenarioTest extends BaseClass2{
 
 		for (int i = 0; i < 3; i++) {
 		    try {
-		        WebElement value5 = wait.until(
+		        WebElement value5 = wait5.until(
 		                ExpectedConditions.visibilityOfElementLocated(valueLocator5)
 		        );
 		        value5.click();
